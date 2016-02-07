6c9dfb3da464bdd4e9f061b340078f364373e036
BorjaL <borja.fpillado@gmail.com>
2016-02-07 16:09:20 +0100

    Start using gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+gulp.task('clean', function(){
+	return gulp.src('web/**/*', {read:false}).pipe(clean())
+});
+
+gulp.task('copy', function() {
+	return gulp.src('src/**/*').pipe(gulp.dest('web'));
+});
+
+gulp.task('default', ['copy']);
